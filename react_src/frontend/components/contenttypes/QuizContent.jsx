8104684d39c24ccdaae33125618e04b398312033
nwuensche <Wuensche.Niklas@gmail.com>
2018-12-05 15:38:04 +0100

    Add TimeStamp Functionality + Change Colors
    Unlock Buttons when new question comes up, lock all buttons if question is over

--- react_src/frontend/components/contenttypes/QuizContent.jsx
+++ react_src/frontend/components/contenttypes/QuizContent.jsx
@@ -13,6 +13,7 @@ class QuizContent extends React.Component {
     this.state = {
       selectedButton: 0,
       enteredButton: -1, // -1 = nothing entered, i = Button i selected
+      timeEnterOver:false,
     };
   }
 
@@ -23,6 +24,29 @@ class QuizContent extends React.Component {
       createHandler(KeyEvent.VK_DOWN, this.down.bind(this)),
       createHandler(KeyEvent.VK_ENTER,  this.enter.bind(this)),
     ]));
+
+    const questions = this.props.questions
+      // New Question Starts
+    questions.map((question, i) => (
+      setTimeout(() => {
+          this.setState(prevState => ({
+                      ...prevState,
+                      selectedButton: 0, //Start at A again
+                      enteredButton: -1, //Unlock Buttons for new Question
+                      timeEnterOver:false //Unlock Buttons for new Question
+
+          }))
+      }, question.start_tms * 1000)));
+        //Question Over
+    questions.map((question, i) => (
+      setTimeout(() => {
+          this.setState(prevState => ({
+                      ...prevState,
+                      timeEnterOver:true // Can't enter a button anymore
+
+          }))
+      }, question.end_tms * 1000)
+        ));
   }
 
   componentWillUnmount() {
@@ -61,8 +85,9 @@ class QuizContent extends React.Component {
         {answers.map((answer, i) => (
              <QuizButton
              item={answer}
-             isSelected={i == this.state.selectedButton}
-             isEntered={i == this.state.enteredButton}
+             isSelected={i === this.state.selectedButton}
+             isEntered={i === this.state.enteredButton}
+             dontSelect={this.state.timeEnterOver || (this.state.enteredButton !== -1)} //Time's up or a button already entered
              />
          ))}
         entered:{this.state.enteredButton}
@@ -71,13 +96,16 @@ class QuizContent extends React.Component {
   }
 }
 
-function QuizButton({ item, isSelected=false, isEntered=false}) {
+function QuizButton({ item, isSelected=false, isEntered=false, dontSelect=false}) {
   let css;
   if (isEntered) {
-    css = { border: '2px #0f0 solid', margin: '10px', padding: '10x' };
+    css = { border: '2px #ffa500 solid', margin: '10px', padding: '10x' };
   }
+    else if (dontSelect) {
+    css = { border: '2px #000 solid', margin: '10px', padding: '10x' };
+    }
     else if (isSelected) {
-    css = { border: '2px #f00 solid', margin: '10px', padding: '10x' };
+    css = { border: '2px #ff0 solid', margin: '10px', padding: '10x' };
   } 
     else { 
      css = { border: '2px #ddd solid', margin: '10px', padding: '10x' }; 
